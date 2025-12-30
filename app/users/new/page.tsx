4fdd09f6337a5b0f7f8c8690b122a87475cd9b81
Masud <wubetumasud@gmail.com>
2025-12-30 14:49:41 +0300

    Routing and Navigation

---
+++ app/users/new/page.tsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const NewUserPage = () => {
+    return (
+        <div>NewUserPage</div>
+    )
+}
+
+export default NewUserPage
---
+++ app/users/page.tsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const UsersPage = () => {
+    return (
+        <div>UsersPage</div>
+    )
+}
+
+export default UsersPage
